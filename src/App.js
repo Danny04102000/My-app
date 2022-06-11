7a47f5bc64a2a73d9ff62dd46f34a99f49131457
cba_danny <danny@cybridge.jp>
2022-06-11 20:18:09 +0700

    update title2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Danny
+          Danny react
         </p>
       </header>
     </div>
